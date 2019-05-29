76fc0dd4d3ec9ba71108589fe32850e1489cbba7
Erik Storm <erikbstorm@outlook.com>
2019-05-29 12:16:58 +0200

    added stuff

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang='en'>
     <?php include 'tpl/head_start.html'; ?>
-    <title>4 op een rij</title>
-    <?php include 'tpl/head_start.html'; ?>
+    <title>Four on a row</title>
+    <?php include 'tpl/head_end.html'; ?>
     <body>
-        
+        <h1>Yeah boii</h1>
     </body>
 </html>
