61b7d8b5d3c6b083c99cb0d4e4a22d4df75ccf94
Erik Storm <erikbstorm@outlook.com>
2019-05-29 12:12:03 +0200

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <?php include 'tpl/head_start.html'; ?>
+    <title>4 op een rij</title>
+    <?php include 'tpl/head_start.html'; ?>
+    <body>
+        
+    </body>
+</html>
