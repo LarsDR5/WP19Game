e660f3c849307e79aeb28736957e3340dfac5c22
Lars de Roo <lars.dr5@gmail.com>
2019-06-03 11:29:14 +0200

    Added buttons and code input field on homepage

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
     <body>
         <?php include 'header.html'; ?>
         <div id='main_div'>
-            <h1>Yeah boii</h1>
+            <div id='under_main_div'>
+                <button type="button" class="btn btn-primary btn-lg btn-block">Generate Code</button>
+                <form>
+                    <div class="form-group">
+                        <label for="codeInput">Enter a code</label>
+                            <input type="text" class="form-control" id="codeInput" placeholder="Enter a code to start a game!">
+                    </div>
+                </form>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Start Game!</button>
+            </div>
         </div>
         <?php include 'footer.html'; ?>
     </body>
