723cc9ae54df3d943255a095085da7dc89b7d7a6
Lars de Roo <lars.dr5@gmail.com>
2019-06-03 11:47:56 +0200

    added some styling for home screen

--- index.php
+++ index.php
@@ -7,14 +7,17 @@
         <?php include 'header.html'; ?>
         <div id='main_div'>
             <div id='under_main_div'>
+                <h1 class="welcomeText">Welcome to Four on a Row!</h1>
+                <h3 class="welcomeText">To start a game with a friend, generate a code...</h3>
                 <button type="button" class="btn btn-primary btn-lg btn-block">Generate Code</button>
+                <h3 class="welcomeText">...or enter a code from a friend!</h3>
                 <form>
                     <div class="form-group">
-                        <label for="codeInput">Enter a code</label>
+                        <label for="codeInput"></label>
                             <input type="text" class="form-control" id="codeInput" placeholder="Enter a code to start a game!">
                     </div>
                 </form>
-                <button type="button" class="btn btn-secondary btn-lg btn-block">Start Game!</button>
+                <button type="button" class="btn btn-primary btn-lg btn-block">Start Game!</button>
             </div>
         </div>
         <?php include 'footer.html'; ?>
