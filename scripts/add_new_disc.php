7455993753b2507f2e453a134144adc5c7fbe466
Erik Storm <erikbstorm@outlook.com>
2019-06-06 11:31:49 +0200

    changed json file

---
+++ scripts/add_new_disc.php
@@ -0,0 +1,8 @@
+<?php
+    $file = file_get_contents('../data/games.json');
+    $content = json_decode($file, true);
+
+    function appendToArray($index){
+        
+    }
+?>
--- scripts/game.js
+++ scripts/game.js
@@ -3,13 +3,23 @@ $(function(){
         $(".table_button").on('click', function() {
             let index = $(this).val();
             console.log(index);
+            let request = $.post('scripts/add_new_disc.php', { 'index': index });
 
-            let request = $.post('scripts/pull_gameboard.php', {'index': index});
-
-            request.done(function(data){
-                $('#game_body').empty();
-                $('#game_body').append(data);
+            request.done(function(){
+                refreshBoard();
             });
         })
     })
+    window.setInterval(function(){
+        
+    }, 1000)
 })
+
+function refreshBoard(){
+    let request = $.post('scripts/pull_gameboard.php');
+
+    request.done(function (data) {
+        $('#game_body').empty();
+        $('#game_body').append(data);
+    });
+}
